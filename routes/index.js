f302d314fcb92316a25aa54c1a4d4eede9e080c1
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2025-02-08 21:16:57 -0700

    just the begin

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ router.get("/", (req, res) => {
     // #swagger.ignore = true
     res.send("<h2>Welcome to the Pirate Project API!</h2>");
   });
-
 router.use("/pirates", require("./pirates"));
 
 //router.use("/api-docs", require("./swagger"));
