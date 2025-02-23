a3cee1d2debf68f8289df123d96e32dafbbca007
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2025-02-22 20:34:23 -0700

    Add OAuth routes and enhance home route with user login options

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
+const homeinfo = `
+"<h2>Welcome to the One Piece Pirate Project API!</h2>
+<h4>Here you can find information about the pirates and ships in the world of One Piece.</h4>
+<p> Sign up or log in to access the API.</p>
+<menu>
+  <ul>
+<menu>
+  <li><button id="logout">logout</button></li>
+  <li><button id="google">Log in with Google</button></li>
+  <li><button id="github">Log in with GitHub</button></li>
+</menu>
+`;
 router.get("/", (req, res) => {
-    // #swagger.ignore = true
-    res.send("<h2>Welcome to the One Piece Pirate Project API!</h2>");
-  });
+  // #swagger.ignore = true
+  res.send(homeinfo);
+});
 router.use("/pirates", require("./pirates"));//pirates
 router.use("/users", require("./users"));//ships
+router.use("/auth", require("./auth"));//auth
 //router.use("/weapons", require("./weapons"))
 
 //router.use("/api-docs", require("./swagger"));
