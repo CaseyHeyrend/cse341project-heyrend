43a62fde457cc1b4e725b8311e6839f0ff632dfc
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2025-03-01 22:29:19 -0700

    Add AUTH0_CLIENT_SECRET to Auth0 configuration

--- src/auth/auth0.js
+++ src/auth/auth0.js
@@ -7,6 +7,7 @@ const config = {
   baseURL: process.env.BASE_URL,
   clientID: process.env.CLIENT_ID,
   issuerBaseURL: process.env.ISSUER_BASE_URL,
+  AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
 };
 
 module.exports = config;
