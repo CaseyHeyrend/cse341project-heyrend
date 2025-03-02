647ccb0f319ef5c9abc7c56990472e020fa59289
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2025-03-01 22:26:39 -0700

    Update secret environment variable to AUTH0_SECRET in auth0.js

--- src/auth/auth0.js
+++ src/auth/auth0.js
@@ -3,7 +3,7 @@ require = require("dotenv").config();
 const config = {
   authRequired: false,
   auth0Logout: true,
-  secret: process.env.SECRET,
+  secret: process.env.AUTH0_SECRET,
   baseURL: process.env.BASE_URL,
   clientID: process.env.CLIENT_ID,
   issuerBaseURL: process.env.ISSUER_BASE_URL,
