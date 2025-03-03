e050fea15804fd7ecdda898e3b4212e6e185f681
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2025-03-02 21:29:24 -0700

    Remove unused utilities import from users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/usersController");
-const utilities = require("../utilities");
 const validation = require("../utilities/user-validation");
 const { requiresAuth } = require("express-openid-connect");
 
